83f5eabb528b0a6c4d2d65228d77f35a859f207e
Mohamed-Hussein-Dev <mohamd.h.yousuf@gmail.com>
2025-09-02 21:15:45 +0300

    fix bugs

--- Web.APIs/obj/Debug/net8.0/Web.APIs.AssemblyInfo.cs
+++ Web.APIs/obj/Debug/net8.0/Web.APIs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a29102c6962ba46f526d4c66749d69d7a6c863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6e817eb120f305e28f919662a3378124cfae67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
